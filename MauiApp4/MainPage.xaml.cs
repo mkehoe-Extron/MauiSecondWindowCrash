5395c572eff22b5f9ec35f200edb10cbd85332f5
mkehoe <mkehoe@extron.com>
2022-10-15 19:04:45 -0700

    Added code to repro bug

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        var window = new Window
+        {
+            Page = new NewPage1()
+        };
+        Application.Current.OpenWindow(window);
+
+    }
+}
+
---
+++ MauiApp4/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using MauiApp4.WinUI;
+
+#endif
+
+namespace MauiApp4;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        _ = builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                _ = fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                _ = fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
